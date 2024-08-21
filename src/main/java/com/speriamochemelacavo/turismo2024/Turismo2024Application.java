44e18b661d5df75e4c49c9d8536444d78b4e36df
ILMaverick <pallottimatteo@gmail.com>
2024-08-22 00:14:14 +0200

    configurazione h2

--- src/main/java/com/speriamochemelacavo/turismo2024/Turismo2024Application.java
+++ src/main/java/com/speriamochemelacavo/turismo2024/Turismo2024Application.java
@@ -3,7 +3,8 @@ package com.speriamochemelacavo.turismo2024;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 public class Turismo2024Application {
 
 	public static void main(String[] args) {
