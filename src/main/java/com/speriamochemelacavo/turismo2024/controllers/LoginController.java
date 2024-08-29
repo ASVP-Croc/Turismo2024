de90f4befd018bcb87dcc40d34f0c3f0e61fda4c
ILMaverick <pallottimatteo@gmail.com>
2024-08-30 00:57:59 +0200

    3 iterazione
    aggiornamento login page + controller

--- src/main/java/com/speriamochemelacavo/turismo2024/controllers/LoginController.java
+++ src/main/java/com/speriamochemelacavo/turismo2024/controllers/LoginController.java
@@ -2,18 +2,20 @@ package com.speriamochemelacavo.turismo2024.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.speriamochemelacavo.turismo2024.services.AccountsService;
 
 @RestController
+@RequestMapping("/login")
 public class LoginController {
 	
 	@Autowired
 	AccountsService accountService;
 	
-	@GetMapping("/login")
+	@GetMapping("/send")
 	public void loginById(@RequestParam(value = "myInput", defaultValue = "101") int id){
 		accountService.saveLoggedUser(id);
 		System.out.println(accountService.userToString(accountService.getLoggedUser()));
